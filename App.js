b8259a05331838e5d44f05355d89cf95cede4554
Eric <ericborges2019@fau.edu>
2022-10-07 09:20:59 -0400

    added Navigation Container

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+  <NavigationContainer>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+	</NavigationContainer>
   );
 }
 
