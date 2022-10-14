eeeeaea9b913ef617e7726a99f99ecb5e53617ff
alexsocial <supermariostarpower4@gmail.com>
2022-10-14 17:17:16 -0400

    Added comment functions for intro + button

--- App.js
+++ App.js
@@ -2,15 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import reactDom from 'react-dom';
+import { TouchableHighlight } from 'react-native-web';
 
+/* INTRODUCTORY ANIMATION EVENT HANDLER
+const intro = new Event('event');
+//listen
+elem.addEventListener('event', (e) => { }, false);
+//dispatch
+elem.dispatchEvent(intro);
+*/
+
+/* BUTTON FUNC + TEXT
+      <TouchableHighlight style={styles.button}>
+      <Text style={styles.buttonText}> I am a button! </Text>
+      </TouchableHighlight>
+*/
 export default function App() {
   return (
   <NavigationContainer>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome, Soldier.</Text>
       <StatusBar style="auto" />
     </View>
-	</NavigationContainer>
+	</NavigationContainer>  
   );
 }
 
@@ -21,4 +36,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonText: {
+    fontSize: 18,
+    color: '#000',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 40,
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
 });
+
