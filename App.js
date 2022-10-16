fd14c165242017b4ccbbdcce3276abbda0d9d1ec
alexsocial <supermariostarpower4@gmail.com>
2022-10-16 12:35:49 -0400

    app rework

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import toplogo from './assets/logo_top.png';
+import bottomlogo from './assets/logo_bottom.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={toplogo} style={styles.logo} /> 
+      <TouchableOpacity /*BUTTON FUNC.*/
+        onPress={() => alert('To be implemented!')}
+        style={{ backgroundColor: 'red' }}>
+        <Text style={{ fontSize: 20, color: '#000' }}>Air Assault Program</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => alert('To be implemented!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#000' }}>Pathfinder Program</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => alert('To be implemented!')}
+        style={{ backgroundColor: 'green' }}>
+        <Text style={{ fontSize: 20, color: '#000' }}>Ranger Program</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => alert('To be implemented!')}
+        style={{ backgroundColor: 'yellow' }}>
+        <Text style={{ fontSize: 20, color: '#000' }}>News</Text>
+      </TouchableOpacity>
+      <Image source={bottomlogo} style={styles.logo} /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +39,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 400,
+    height:200
   },
 });
