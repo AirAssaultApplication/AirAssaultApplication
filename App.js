f715c98857026a063cf82ffb7954b1a4559f33a4
alexsocial <achaban1001@gmail.com>
2022-10-27 18:04:49 -0400

    App upgrade

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import toplogo from './assets/logo_top.png';
 import bottomlogo from './assets/logo_bottom.png';
+/*font stuff
+import { useFonts } from "expo-font";
+
+const Home = () => {
+  let [fontsLoaded] = useFonts ({
+    "Arial-Black":  require("./assets/fontfam.ttf"),
+  });
+}
+*/
 
 export default function App() {
   return (
@@ -10,22 +19,22 @@ export default function App() {
       <TouchableOpacity /*BUTTON FUNC.*/
         onPress={() => alert('To be implemented!')}
         style={{ backgroundColor: '#212121' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Air Assault Program</Text>
+        <Text style={styles.homebutton}>Air Assault Program</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => alert('To be implemented!')}
         style={{ backgroundColor: '#212121' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pathfinder Program</Text>
+        <Text style={styles.homebutton}>Pathfinder Program</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => alert('To be implemented!')}
         style={{ backgroundColor: '#212121' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Ranger Program</Text>
+        <Text style={styles.homebutton}>Ranger Program</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => alert('To be implemented!')}
         style={{ backgroundColor: '#212121' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>News</Text>
+        <Text style={styles.homebutton}>News</Text>
       </TouchableOpacity>
       <Image source={bottomlogo} style={styles.logo} /> 
       <StatusBar style="auto" />
@@ -36,12 +45,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212121',
     alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
-    width: 400,
-    height:200
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+  },
+  homebutton: {
+    fontSize: 20,
+    color: '#fff',
+    borderColor: '#EDB402',
+    borderWidth: 2,
+    backgroundColor: '#2B2B2B',
+    borderRadius: 4,
+    width: 300,
+    height: 100,
+    textAlign: 'center',
+    textAlignVertical: 'center',
   },
 });
