6190fff732be0755883f14d74811e731a55a3aa1
Eric <ericborges2019@fau.edu>
2022-11-07 12:16:31 -0500

    Refactored app to use React Navigation

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import toplogo from './assets/logo_top.png';
 import bottomlogo from './assets/logo_bottom.png';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {DetailsScreen} from './PathFinderHome.js';
+
 /*font stuff
 import { useFonts } from "expo-font";
 
@@ -12,7 +17,9 @@ const Home = () => {
 }
 */
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Image source={toplogo} style={styles.logo} /> 
@@ -21,49 +28,71 @@ export default function App() {
         style={{ backgroundColor: '#212121' }}>
         <Text style={styles.homebutton}>Air Assault Program</Text>
       </TouchableOpacity>
+
       <TouchableOpacity
-        onPress={() => alert('To be implemented!')}
+        onPress={() => navigation.navigate('Details')}
         style={{ backgroundColor: '#212121' }}>
         <Text style={styles.homebutton}>Pathfinder Program</Text>
       </TouchableOpacity>
+
       <TouchableOpacity
         onPress={() => alert('To be implemented!')}
         style={{ backgroundColor: '#212121' }}>
         <Text style={styles.homebutton}>Ranger Program</Text>
       </TouchableOpacity>
+
       <TouchableOpacity
         onPress={() => alert('To be implemented!')}
         style={{ backgroundColor: '#212121' }}>
         <Text style={styles.homebutton}>News</Text>
       </TouchableOpacity>
+
       <Image source={bottomlogo} style={styles.logo} /> 
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#212121',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'contain',
-  },
-  homebutton: {
-    fontSize: 20,
-    color: '#fff',
-    borderColor: '#EDB402',
-    borderWidth: 2,
-    backgroundColor: '#2B2B2B',
-    borderRadius: 4,
-    width: 300,
-    height: 100,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-  },
-});
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      
+      <Stack.Navigator initialRouteName="Home">
+
+        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+    );
+  }
+
+      const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: '#212121',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        logo: {
+          width: '100%',
+          height: 200,
+          resizeMode: 'contain',
+        },
+        homebutton: {
+          fontSize: 20,
+          color: '#fff',
+          borderColor: '#EDB402',
+          borderWidth: 2,
+          backgroundColor: '#2B2B2B',
+          borderRadius: 4,
+          width: 300,
+          height: 100,
+          textAlign: 'center',
+          textAlignVertical: 'center',
+        },
+      });
+
