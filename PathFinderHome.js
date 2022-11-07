c46342c12e7264805bea5fa02812aed54bc422dd
Eric <ericborges2019@fau.edu>
2022-11-07 12:16:31 -0500

    Refactored app to use React Navigation
    bugs:
    	*PathFinderHome page is blank.

---
+++ PathFinderHome.js
@@ -0,0 +1,7 @@
+export function DetailsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen what</Text>
+      </View>
+    );
+  }
