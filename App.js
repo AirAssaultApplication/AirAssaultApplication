7373861be0ab9a3ed4202e47dac2fa80ff89f43f
RenanQueiroz <queirozrrq@gmail.com>
2022-11-11 20:36:06 -0500

    React Native Paper initial implementation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import toplogo from './assets/logo_top.png';
 import bottomlogo from './assets/logo_bottom.png';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MD3DarkTheme as DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import {DetailsScreen} from './PathFinderHome.js';
 
@@ -17,56 +18,66 @@ const Home = () => {
 }
 */
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
 const Stack = createNativeStackNavigator();
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Image source={toplogo} style={styles.logo} /> 
-      <TouchableOpacity /*BUTTON FUNC.*/
-        onPress={() => alert('To be implemented!')}
-        style={{ backgroundColor: '#212121' }}>
-        <Text style={styles.homebutton}>Air Assault Program</Text>
-      </TouchableOpacity>
+        <Image source={toplogo} style={styles.logo} />
+        <TouchableOpacity /*BUTTON FUNC.*/
+          onPress={() => alert('To be implemented!')}
+          style={{ backgroundColor: '#212121' }}>
+          <Text style={styles.homebutton}>Air Assault Program</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Details')}
-        style={{ backgroundColor: '#212121' }}>
-        <Text style={styles.homebutton}>Pathfinder Program</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Details')}
+          style={{ backgroundColor: '#212121' }}>
+          <Text style={styles.homebutton}>Pathfinder Program</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => alert('To be implemented!')}
-        style={{ backgroundColor: '#212121' }}>
-        <Text style={styles.homebutton}>Ranger Program</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => alert('To be implemented!')}
+          style={{ backgroundColor: '#212121' }}>
+          <Text style={styles.homebutton}>Ranger Program</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => alert('To be implemented!')}
-        style={{ backgroundColor: '#212121' }}>
-        <Text style={styles.homebutton}>News</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => alert('To be implemented!')}
+          style={{ backgroundColor: '#212121' }}>
+          <Text style={styles.homebutton}>News</Text>
+        </TouchableOpacity>
 
-      <Image source={bottomlogo} style={styles.logo} /> 
-      <StatusBar style="auto" />
-    </View>
+        <Image source={bottomlogo} style={styles.logo} />
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      
-      <Stack.Navigator initialRouteName="Home">
+    <PaperProvider>
+      <NavigationContainer>
+        
+        <Stack.Navigator initialRouteName="Home">
 
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
 
-      </Stack.Navigator>
+        </Stack.Navigator>
 
-    </NavigationContainer>
-
+      </NavigationContainer>
+    </PaperProvider>
     );
   }
 
--- PathFinderHome.js
+++ PathFinderHome.js
@@ -1,10 +1,43 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import { BottomNavigation, Text } from 'react-native-paper';
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => <Text>Notifications</Text>;
+
 
 export function DetailsScreen() {
+    const theme = useTheme();
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+      { key: 'music', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline'},
+      { key: 'albums', title: 'Albums', focusedIcon: 'album' },
+      { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+      { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+    ]);
+  
+    const renderScene = BottomNavigation.SceneMap({
+      music: MusicRoute,
+      albums: AlbumsRoute,
+      recents: RecentsRoute,
+      notifications: NotificationsRoute,
+    });
+  
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', backgroundColor: theme.colors.background }}>
+        
         <Text>Details Screen what</Text>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
       </View>
     );
   }
