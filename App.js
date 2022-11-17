3ec5d66492047dded99358f8bd5a9f4131b11d5f
Kevin Tudor <114966429+KT-FAU@users.noreply.github.com>
2022-11-17 17:49:41 -0500

    Updated splash to 17.5 | hide Navbar

--- App.js
+++ App.js
@@ -58,7 +58,8 @@ export default function App() {
   return (
     <NavigationContainer>
       
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
 
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
