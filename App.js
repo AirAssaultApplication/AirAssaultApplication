12740c56e4c148c3374acc324127a65f0a854c42
RenanQueiroz <queirozrrq@gmail.com>
2023-01-23 16:19:25 -0500

    Implement React Native Gesture Handler
    * This makes it so gestures are no longer controlled by the JS responder system, instead being handled in the UI thread

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
