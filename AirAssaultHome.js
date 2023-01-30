6f149a14f2f6a7e68a3a55350d0ccc1661a5614e
RenanQueiroz <queirozrrq@gmail.com>
2023-01-30 14:57:47 -0500

    Rework code to add support for dynamically adding new flashcards to the screen from an object array
    * Flashcard items require an id value so they can be referenced later, this is per React Native documentation for using views from arrays

--- AirAssaultHome.js
+++ AirAssaultHome.js
@@ -42,101 +42,71 @@ const styles = StyleSheet.create({
   },
 });
 
-
+export function createFlashcard(flashcard){
+  const theme = useTheme();
+  return(
+    <View style={styles.card} key={flashcard.id}>
+      <TouchableRipple
+        onPress={() => console.log('pressed')}
+        borderless={true}
+        style={styles.cardBtn}
+      >
+        <Card mode='contained'>
+          <Card.Content>
+            <View style={{flexDirection:'row'}}>
+              <View style={{flex:1}}>
+                <View style={{justifyContent: 'flex-start'}}>
+                  <Text variant='titleMedium'>{flashcard.question}</Text>
+                </View>
+              </View>
+              <View>
+                <View style={{justifyContent: 'flex-end', marginTop: 8}}>
+                  <Button icon='chevron-right' contentStyle={{flexDirection: 'row-reverse'}} style={{marginHorizontal: -8}}>
+                  </Button>
+                </View>
+              </View>
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableRipple>
+    </View>
+  );
+}
 
 export function AirAssaultScreen() {
-    const theme = useTheme();
-    const cardQuestions = [
-      'What is the allowable cargo load of the LUH-72A in its normal configuration when used for MEDEVAC operations?',
-      'What is the primary use for the CH-47 when used during CASEVAC missions?',
-      'What is the crew of a LUH- 72A when not being used for MEDEVAC operations?',
-    ];
-    const cardAnswers = [
-      '2 litter, 1 medic, and 5 ambulatory.',
-      'Mass Casualty Evacuation.',
-      '(4) Pilot, Co Pilot, Crew Chief, and In-Flight Medic.',
-    ];
-    return (
-      <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
-      <View style={{marginTop: 8, marginBottom: 8}}>
-        <Text variant='headlineSmall' style={{marginBottom:8}}>The Sabalauski Air Assault School</Text>
-        <Text variant='headlineMedium'>Phase I</Text>
-      </View>
-      <View style={styles.card}>
-        <TouchableRipple
-          onPress={() => console.log('pressed')}
-          borderless={true}
-          style={styles.cardBtn}
-        >
-          <Card mode='contained'>
-            <Card.Content>
-              <View style={{flexDirection:'row'}}>
-                <View style={{flex:1}}>
-                  <View style={{justifyContent: 'flex-start'}}>
-                    <Text variant='titleMedium'>{cardQuestions[0]}</Text>
-                  </View>
-                </View>
-                <View>
-                  <View style={{justifyContent: 'flex-end', marginTop: 8}}>
-                    <Button icon='chevron-right' contentStyle={{flexDirection: 'row-reverse'}} style={{marginHorizontal: -8}}>
-                    </Button>
-                  </View>
-                </View>
-              </View>
-            </Card.Content>
-          </Card>
-        </TouchableRipple>
-      </View>
-      <View style={styles.card}>
-        <TouchableRipple
-          onPress={() => console.log('pressed')}
-          borderless={true}
-          style={styles.cardBtn}
-        >
-          <Card mode='contained'>
-            <Card.Content>
-              <View style={{flexDirection:'row'}}>
-                <View style={{flex:1}}>
-                  <View style={{justifyContent: 'flex-start'}}>
-                    <Text variant='titleMedium'>{cardQuestions[1]}</Text>
-                  </View>
-                </View>
-                <View>
-                  <View style={{justifyContent: 'flex-end', marginTop: 8}}>
-                    <Button icon='chevron-right' contentStyle={{flexDirection: 'row-reverse'}} style={{marginHorizontal: -8}}>
-                    </Button>
-                  </View>
-                </View>
-              </View>
-            </Card.Content>
-          </Card>
-        </TouchableRipple>
-      </View>
-      <View style={styles.card}>
-        <TouchableRipple
-          onPress={() => console.log('pressed')}
-          borderless={true}
-          style={styles.cardBtn}
-        >
-          <Card mode='contained'>
-            <Card.Content>
-              <View style={{flexDirection:'row'}}>
-                <View style={{flex:1}}>
-                  <View style={{justifyContent: 'flex-start'}}>
-                    <Text variant='titleMedium'>{cardQuestions[2]}</Text>
-                  </View>
-                </View>
-                <View>
-                  <View style={{justifyContent: 'flex-end', marginTop: 8}}>
-                    <Button icon='chevron-right' contentStyle={{flexDirection: 'row-reverse'}} style={{marginHorizontal: -8}}>
-                    </Button>
-                  </View>
-                </View>
-              </View>
-            </Card.Content>
-          </Card>
-        </TouchableRipple>
-      </View>
-      </ScrollView>
-    );
+  const theme = useTheme();
+
+  let flashcards = [
+    {
+      "id": 0,
+      "question": "What is the allowable cargo load of the LUH-72A in its normal configuration when used for MEDEVAC operations?",
+      "answer": "2 litter, 1 medic, and 5 ambulatory."
+    },
+    {
+      "id": 1,
+      "question": "What is the primary use for the CH-47 when used during CASEVAC missions?",
+      "answer": "Mass Casualty Evacuation."
+    },
+    {
+      "id": 2,
+      "question": "What is the crew of a LUH- 72A when not being used for MEDEVAC operations?",
+      "answer": "(4) Pilot, Co Pilot, Crew Chief, and In-Flight Medic."
+    },
+  ];
+
+  let flashcardViews = [];
+
+  for(const item of flashcards){
+    flashcardViews.push(createFlashcard(item));
   }
+
+  return (
+    <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+    <View style={{marginTop: 8, marginBottom: 8}}>
+      <Text variant='headlineSmall' style={{marginBottom:8}}>The Sabalauski Air Assault School</Text>
+      <Text variant='headlineMedium'>Phase I</Text>
+    </View>
+    {flashcardViews}
+    </ScrollView>
+  );
+}
